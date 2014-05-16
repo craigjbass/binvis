51318235727d8a6ff0eea1c577d58972c0630c6f
Craig Bass <craig@devls.co.uk>
2014-05-16 23:20:36 +0100

    Init

---
+++ desktop/src/uk/co/craigbass/binvis/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.co.craigbass.binvis.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.co.craigbass.binvis.BinVis;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BinVis(), config);
+	}
+}
